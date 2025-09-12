6bc36e8dd7b07a75bc33f43c1e5c8d6c8ab4aace
gourav-1711 <gouravdadhich34@gmail.com>
2025-09-12 12:14:54 +0530

    key removed

--- src/app/(firebase)/firebase.config.js
+++ src/app/(firebase)/firebase.config.js
@@ -17,13 +17,3 @@ console.log(firebaseConfig , process.env.NEXT_PUBLIC_FIREBASE_API_KEY);
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAtbDfKWnYc2Kx_THyx2efBDkfVWfWNiow",
-//   authDomain: "song-64d4a.firebaseapp.com",
-//   databaseURL: "https://song-64d4a-default-rtdb.firebaseio.com",
-//   projectId: "song-64d4a",
-//   storageBucket: "song-64d4a.firebasestorage.app",
-//   messagingSenderId: "514910186591",
-//   appId: "1:514910186591:web:15af49e87cf98cea053fea"
-// };
