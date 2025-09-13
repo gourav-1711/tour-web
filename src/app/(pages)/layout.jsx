ce267f496e3ce3814d2dd7c03ed201518e3c776b
gourav-1711 <gouravdadhich34@gmail.com>
2025-09-13 12:38:22 +0530

    rename header

--- src/app/(pages)/layout.jsx
+++ src/app/(pages)/layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Header } from "@/components/Header";
 import EnhancedFooter from "@/components/EnhancedFooter";
+import { Header } from "@/components/header";
+
 
 export default function layout({ children }) {
   return (
